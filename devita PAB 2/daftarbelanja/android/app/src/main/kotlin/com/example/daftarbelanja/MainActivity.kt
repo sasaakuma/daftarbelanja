fae2994a25f49b6fe1958605b792b4e8cb957bfd
iniJesen <>
2024-03-21 09:29:59 +0700

    commit add & delete daftar belanja

---
+++ devita PAB 2/daftarbelanja/android/app/src/main/kotlin/com/example/daftarbelanja/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.daftarbelanja
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ devita PAB 2/daftarbelanja/lib/services/shopping_service.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_database/firebase_database.dart';
+
+class ShoppingService {
+  final DatabaseReference _database =
+      FirebaseDatabase.instance.ref().child('shopping_list');
+
+  Stream<Map<String, String>> getShoppingList() {
+    return _database.onValue.map((event) {
+      final Map<String, String> items = {};
+      DataSnapshot snapshot = event.snapshot;
+      print('Snapshot data: ${snapshot.value}');
+
+      if (snapshot.value != null) {
+        Map<dynamic, dynamic> values = snapshot.value as Map<dynamic, dynamic>;
+        values.forEach((key, value) {
+          items[key] = value['name'] as String;
+        });
+      }
+
+      return items;
+    });
+  }
+
+  // untuk menambah kan data
+  void addShoppingList(String itemName) {
+    _database.push().set({'name': itemName});
+  }
+
+  // untuk menghapus data
+  Future<void> removeShoppingItem(String key) async {
+    await _database.child(key).remove();
+  }
+}
